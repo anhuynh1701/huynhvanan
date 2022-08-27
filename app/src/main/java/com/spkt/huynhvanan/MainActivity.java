0eabec3b74940372c084b5a58238407bd8393793
anhuynh1701 <chincutephomnai@gmail.com>
2022-08-27 22:41:28 +0700

    first commit

---
+++ app/src/main/java/com/spkt/huynhvanan/MainActivity.java
@@ -0,0 +1,14 @@
+package com.spkt.huynhvanan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
